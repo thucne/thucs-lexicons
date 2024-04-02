9c09f96e76fcba4ad740051c4d37eb03aef5b5ec
Tran Trong Thuc <trongthuc.bentre@gmail.com>
2024-04-02 13:07:43 +0700

    hotfix, add suspense boundary

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,21 @@
 'use client';
-import React from 'react';
+
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-const SearchPage = () => {
+function Search() {
     const searchParams = useSearchParams();
-
     const query = searchParams.get('q');
 
     return <div>Hello, this is the search page for query: {query}</div>;
+}
+
+const SearchPage = () => {
+    return (
+        <Suspense>
+            <Search />
+        </Suspense>
+    );
 };
 
 export default SearchPage;
