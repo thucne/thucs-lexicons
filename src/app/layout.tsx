dfe244e31a654178f3d482bdca85cd434bb72fb3
Tran Trong Thuc <trongthuc.bentre@gmail.com>
2024-04-03 18:01:35 +0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['vietnamese', 'latin'] });
 
 export const metadata: Metadata = {
     title: 'My Lexicon',
-    description: 'This app is used to store and manage vocabulary.',
+    description: `Search for word's definition, synonyms, antonyms, and more...`,
     openGraph: {
         images: [
             {
