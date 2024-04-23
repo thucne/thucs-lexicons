0c6da7b1723f411fe6434aa0aa1f11fc0394f5bc
Tran Trong Thuc <trongthuc.bentre@gmail.com>
2024-04-23 09:38:28 +0700

    optimize loading

--- src/components/organisms/SearchPage/index.tsx
+++ src/components/organisms/SearchPage/index.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation';
 import { useLexicon } from '@/hooks/use-lexicon';
 import { useAppDispatch, useAppSelector } from '@/redux/store';
 import { SearchResultsState, selectSearchResults, setSearchResults } from '@/redux/reducers/searchResults';
+import { CircularProgress, Typography } from '@mui/material';
 
 type SearchPageProps = {
     word: string;
@@ -22,7 +23,12 @@ const SearchPageBody = ({ word }: SearchPageProps) => {
     });
 
     if (isLoading) {
-        return <div>Fetching data...</div>;
+        return (
+            <Typography className="flex items-center gap-2">
+                Fetching data
+                <CircularProgress size={16} />
+            </Typography>
+        );
     }
 
     if (error) {
