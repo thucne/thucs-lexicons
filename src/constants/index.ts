5ea4327255e72bb00725cefeb696dd8fe06f9126
Tran Trong Thuc <trongthuc.bentre@gmail.com>
2025-05-26 16:20:55 +0700

    og

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const FREE_DICTIONARY_API = 'https://api.dictionaryapi.dev/api/v2/entries
 export const THUCDEDEV_BANNER_PNG = '/thucdedev-banner.png';
 export const THUCDEDEV_BANNER_PNG_DARK = '/thucdedev-banner.dark.png';
 export const DEFAULT_OG_IMAGE =
-    'https://res.cloudinary.com/katyperrycbt/image/upload/v1712247748/n4srlgejymk1hsufxpze.jpg';
+    'https://res.cloudinary.com/katyperrycbt/image/upload/v1748251242/j0xe4krtuggdshxbsnyb.jpg';
 export const GET_SUPABASE_WORDS_URL = '/api/freedictionaryapi/get';
 // export const DYNAMIC_OG_BG = 'https://res.cloudinary.com/katyperrycbt/image/upload/v1723636982/vkwvdhxz54adjppfrchx.jpg'
 // export const DYNAMIC_OG_BG = 'https://res.cloudinary.com/katyperrycbt/image/upload/v1748245946/zbaeledlaymaiwbr7ett.jpg'
